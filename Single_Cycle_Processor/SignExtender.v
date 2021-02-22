b4b9d588a380755f5dfea98457f4918a8ca7f537
ronhadri12 <78065699+ronhadri12@users.noreply.github.com>
2021-02-21 22:16:19 -0600

    Update SignExtender.v

--- Single_Cycle_Processor/SignExtender.v
+++ Single_Cycle_Processor/SignExtender.v
@@ -7,16 +7,17 @@ input [31:0] Inst;
 
 always @(*)
 begin
-if(Inst[31:26] == 6'b000101 || Inst[31:26] == 6'b100101) //opcodes for B
-	BusImm = {{38{Inst[25]}},Inst[25:0]}; //Type B
+	if(Inst[31:26] == 6'b000101 || Inst[31:26] == 6'b100101) //opcodes for B
+		BusImm = {{38{Inst[25]}},Inst[25:0]}; //Type B
 
-else if (Inst[31:24] == 8'b01010100 || Inst[31:24] == 8'b10110100 || Inst[31:24] == 8'b10110101) //opcodes for cb
-	BusImm = {{45{Inst[23]}},Inst[23:5]}; //Type CB
+	else if (Inst[31:24] == 8'b01010100 || Inst[31:24] == 8'b10110100 || Inst[31:24] == 8'b10110101) //opcodes for cb
+		BusImm = {{45{Inst[23]}},Inst[23:5]}; //Type CB
 
-else if (Inst[31:21] == 11'b00111000000 || Inst[31:21] == 11'b00111000010 || Inst[31:21] == 	 	11'b01111000000 || Inst[31:21] == 11'b01111000010 || Inst[31:21] == 11'b11111000000 || 		Inst[31:21] == 11'b11111000010) //opcode for D
-	BusImm = {{55{Inst[20]}},Inst[20:12]}; //Type D
-else
-	BusImm = 64'b0; //default
+	else if (Inst[31:21] == 11'b00111000000 || Inst[31:21] == 11'b00111000010 || Inst[31:21] == 11'b01111000000 || 
+		 Inst[31:21] == 11'b01111000010 || Inst[31:21] == 11'b11111000000 || Inst[31:21] == 11'b11111000010) //opcode for D
+		BusImm = {{55{Inst[20]}},Inst[20:12]}; //Type D
+	else
+		BusImm = 64'b0; //default
 end
 
 
